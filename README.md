bac468a5aacef52691ea4d098fb6a44345994a85
KevinPham01 <130538243+KevinPham01@users.noreply.github.com>
2024-09-08 14:59:32 -0400

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # COT-4655-Lab-2
 
+## What I Learned
+* Setting up a Parse Server: Configured a custom remote Parse backend server to handle authentication and data storage.
+* Parse Swift SDK Integration: Successfully integrated the Parse Swift SDK into the project for seamless communication between the app and the server.
+* User Authentication: Implemented user sign-up, login, and logout functionalities using Parse Swift, ensuring credentials persist across app launches.
+* Photo Upload: Enabled users to choose a photo from their photo library and upload it with a caption to the server, with posts displaying in a personalized feed.
+
+## Features
+* Sign up and create an account.
+* Log in and log out.
+* Persist login credentials on multiple app launches.
+* Create a post with a photo and caption.
+* View posts in the user’s feed.
+
 ![ezgif com-video-to-gif-converter (7)](https://github.com/user-attachments/assets/fc50b801-4101-4b35-afce-78cddd75e4b5)
--- lab-insta-parse/AppDelegate.swift
+++ lab-insta-parse/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import ParseSwift
 // TODO: Pt 1 - Import Parse Swift
 
 @main
@@ -16,13 +16,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
 
         // TODO: Pt 1 - Initialize Parse SDK
-        // https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
+        //
+        ParseSwift.initialize(applicationId: "brPmkQcsFoor7kI8WmDxawCw63iWjhnSFgeBEvh8",
+                              clientKey: "Z2nMhCl6QF5sekVzz0AvUNYjDsXFNcPBuJtbGX9p",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
 
-
+        
         // TODO: Pt 1: - Instantiate and save a test parse object to your server
         // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L121
 
+        // Instantiate the test parse object
+        var score = GameScore()
+        score.playerName = "Kingsley"
+        score.points = 13
 
+        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+        // If no callbackQueue is specified it returns to main queue.
+        score.save { result in
+            switch result {
+            case .success(let savedScore):
+                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+            case .failure(let error):
+                assertionFailure("Error saving: \(error)")
+            }
+        }
+        
         return true
     }
 
@@ -44,4 +62,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // TODO: Pt 1 - Create Test Parse Object
 // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
 
+// Create your own value type `ParseObject`.
+struct GameScore: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
 
+    // Your own custom properties.
+    // All custom properties must be optional.
+    var playerName: String?
+    var points: Int?
+}
